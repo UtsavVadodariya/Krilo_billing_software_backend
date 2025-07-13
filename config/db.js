1a7c99abca890ab97651cdf70bd4d82289100494
UtsavVadodariya <162439323+UtsavVadodariya@users.noreply.github.com>
2025-07-13 09:04:53 +0530

    backend3

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb+srv://utsavvadodariya2008:UtsavUtsav%40162@cluster0.8a3idtg.mongodb.net/billing-software?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("mongodb+srv://utsavvadodariya2008:Utsav%40162@cluster0.8a3idtg.mongodb.net/billing-software?retryWrites=true&w=majority&appName=Cluster0");
 
 const db = mongoose.connection;
 
