b4c233f99af8a4f107f3ddc4e26c65a450fa3fa9
UtsavVadodariya <162439323+UtsavVadodariya@users.noreply.github.com>
2025-08-07 22:45:25 +0530

    backend-07

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,12 +1,12 @@
 // const baseUrl = 'http://localhost:5000';
 
 
-export const baseUrl = 'https://krilobilling.easywayitsolutions.com';
+const baseUrl = 'https://krilobilling.easywayitsolutions.com';
 // const mongooseDB = 'mongodb://127.0.0.1/billing-software';
-export const mongooseDB = 'mongodb+srv://utsavvadodariya2008:Utsav%40162@cluster0.8a3idtg.mongodb.net/billing-software?retryWrites=true&w=majority&appName=Cluster0';
+const mongooseDB = 'mongodb+srv://utsavvadodariya2008:Utsav%40162@cluster0.8a3idtg.mongodb.net/billing-software?retryWrites=true&w=majority&appName=Cluster0';
 
 // const mongooseConnectIndex = 'mongodb://localhost:27017/krilo_billing_software';
-export const mongooseConnectIndex = 'mongodb+srv://utsavvadodariya2008:Utsav%40162@cluster0.8a3idtg.mongodb.net/krilo_billing_software?retryWrites=true&w=majority&appName=Cluster0';
+const mongooseConnectIndex = 'mongodb+srv://utsavvadodariya2008:Utsav%40162@cluster0.8a3idtg.mongodb.net/krilo_billing_software?retryWrites=true&w=majority&appName=Cluster0';
 
 
 module.exports = {
